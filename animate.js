7323547814376733ba60efe89322306d028d180f
fhaque1 <farhan3231@gmail.com>
2017-03-04 12:25:54 -0500

    finished Circle Growing-Shrinking

--- animate.js
+++ animate.js
@@ -4,33 +4,24 @@ var ns = "http://www.w3.org/2000/svg"
 var rid = 0;
 var height = svgarea.getAttribute("height");
 var width = svgarea.getAttribute("width");
+var small = Math.min(height, width);
 
 var animateDot = function(e) {
 	var x = 0;
 	window.cancelAnimationFrame(rid);
 	var circle = document.createElementNS(ns, "circle");
-	circle.setAttributeNS(null, "cx", height/2);
+	circle.setAttributeNS(null, "cx", width/2);
 	circle.setAttributeNS(null, "cy", height/2);
-	circle.setAttributeNS(null, "fill", "red");
+	circle.setAttributeNS(null, "fill", "blue");
+	var dir = 1;
 	var growDot = function(e) {
 		c.innerHTML = "";
 		console.log(rid);	
 		circle.setAttributeNS(null, "r",  x);
 		c.appendChild(circle);
-		x++;
-		if (x > height/2){
-			return shrinkDot();		
-		}
-		rid = window.requestAnimationFrame(growDot);
-	};
-	var shrinkDot = function(e) {
-		c.innerHTML = "";
-		console.log(rid);	
-		circle.setAttributeNS(null, "r",  x);
-		c.appendChild(circle);
-		x--;
-		if (x <= 0){
-			return growDot();		
+		x += dir;
+		if (x > small/2 || x <= 0){
+			dir *= -1;		
 		}
 		rid = window.requestAnimationFrame(growDot);
 	};
